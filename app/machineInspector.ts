1fd06060d489ed33eef62edae48e128b4e3b20c4
Brandon Lorenz <blorenz@gmail.com>
2024-01-27 12:08:46 -0500

    updated

--- app/machineInspector.ts
+++ app/machineInspector.ts
@@ -3,7 +3,7 @@ import { createBrowserInspector } from "@statelyai/inspect";
 const inspector = () => {
   if (typeof window !== "undefined") {
     /// Returning this will enable the inspector in the browser
-    // return createBrowserInspector();
+    return createBrowserInspector();
   }
   return { inspect: undefined };
 };
